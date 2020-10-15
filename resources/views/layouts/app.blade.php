675bc632a015f99802f902952ad3012ef1dde32e
SovannthaiNEANG <44717540+SovannthaiNEANG@users.noreply.github.com>
2020-10-15 12:59:49 +0700

    crud item food

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('backend/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('backend/css/material-dashboard.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    @stack('css')
+</head>
+<body>
+    <div id="app">
+        <div class="wrapper">
+            @if(Request::is('admin*'))
+                @include('layouts.partial.sidebar')
+            @endif
+            <div class="main-panel">
+                @if(Request::is('admin*'))
+                    @include('layouts.partial.topbar')
+                @endif
+                    @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('backend/js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('backend/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('backend/js/material.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('backend/js/chartist.min.js') }}"></script>
+    <script src="{{ asset('backend/js/arrive.min.js') }}"></script>
+    <script src="{{ asset('backend/js/perfect-scrollbar.jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/js/bootstrap-notify.js') }}"></script>
+    <script src="{{ asset('backend/js/material-dashboard.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    {!! Toastr::message() !!}
+    @stack('scripts')
+</body>
+</html>
